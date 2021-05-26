4e762d115738f12f90bbaf1d9265724c5c0269c5
Fabien Sanchez <fabien.sanchez@up.coop>
2021-05-26 16:13:16 +0200

    feat | test, mise en place des test case de base

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+
+/**
+ * Class TestCase pour la base des tests du projet
+ * @package Test
+ */
+class TestCase extends PhpUnitTestCase
+{
+
+}
